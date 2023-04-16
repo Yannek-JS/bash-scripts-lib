875d8c8ea5834a24e030327d2a5d020d9ee74f4d
Yannek-JS <yannek@yannek.pro>
2023-04-16 16:31:11 +0100

    Add an equal mark into if condition in yes_or_not function. Add press_any_key function.

--- bash-scripts-lib.sh
+++ bash-scripts-lib.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+###############################################################
+# This script contains the Bash code portions commonly 
+# used in other Yannek-JS Bash projects.
+#
+# Visit https://github.com/Yannek-JS/bash-scripts-lib
+# for the most recent script release
+############################################################### 
+
 # the colours definitions
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -13,7 +21,7 @@ SC='\033[0m' # Standard colour
 
 function draw_line() {  # it draws a line made of 80 hyphens
     for num in $(seq 0 79); do echo -n '-'; done
-            echo
+    echo
 }
 
 
@@ -30,7 +38,7 @@ function yes_or_not() {  # gives you a choice: to continue or to quit this scrip
     do
         echo -e -n '\nIf you want to continue type '${LRED}'Yes'${SC}'. Otherwise type '${LGREEN}'No'${SC}': '
         read yn
-        if [[ "${yn,,}" = "no" ]]; then quit_now; fi
+        if [[ "${yn,,}" == "no" ]]; then quit_now; fi
     done
 }
 
@@ -42,4 +50,12 @@ function check_if_root() {  # checks if script is being run by a user having roo
         quit_now
     fi
 }
+
+
+function press_any_key() {
+    echo -e -n ${ORANGE}'\n Press any key to continue...'${SC}
+    read -n1 -s anyKey
+}
+
+
 
